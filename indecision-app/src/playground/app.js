e9a12f3ef85e520c0ac4d1698760d96f8753b263
Adam Speer <adam@pop-os.localdomain>
2021-07-21 18:15:41 -0400

    added new config, section 6 module 54

--- indecision-app/src/playground/app.js
+++ indecision-app/src/playground/app.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddOption from './components/AddOption';
-import Option from './components/Option';
-
 class IndecisionApp extends React.Component {
   constructor(props) {
     super(props);
@@ -134,5 +129,56 @@ const Options = (props) => {
     </div>
   );
 };
+
+const Option = (props) => {
+  return (
+    <li>
+      {props.optionText}
+      <button
+        onClick={(e) => {
+          props.handleDeleteOption(props.optionText);
+        }}
+      >
+        Remove
+      </button>
+    </li>
+  );
+};
+
+class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined,
+    };
+  }
+
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+    const error = this.props.handleAddOption(option);
+    e.target.elements.option.value = '';
+
+    this.setState(() => ({ error: error }));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.handleAddOption}>
+          <input type='text' name='option' />
+          <button>Add Option</button>
+        </form>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ indecision-app/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+console.log(__dirname);
+
+module.exports = {
+  entry: './src/app.js',
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
